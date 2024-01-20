83ac9c0c0fff6efaa985c3b0c8364a0a12cae39c
Thien Hung <140578137+hungnthe5@users.noreply.github.com>
2024-01-20 13:42:55 +0700

    Update not-paid.js

--- not-paid.js
+++ not-paid.js
@@ -1,7 +1,7 @@
 (function () {
   /* change these variables as you wish */
   var due_date = new Date("2024-01-18");
-  var days_deadline = 10;
+  var days_deadline = 7;
   /* stop changing here */
 
   var current_date = new Date();
